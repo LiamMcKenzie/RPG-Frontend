ec495575719b4590332b0df1f7efd1f7a28517f0
Liam McKenzie (1000103822) <MCKELD3@student.op.ac.nz>
2023-07-31 13:47:36 +1200

    added unity project

---
+++ Studio-5-Unity/Assets/DisplayText.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayText : MonoBehaviour
+{
+    public TMP_Text tmpText;
+    public APITest apiTest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tmpText.text = apiTest.dataWrapper.data.symbol;
+    }
+}
