f026b2af582c7c24e52d0529acdb88358689a038
Devon Partridge-Officer <90590089+DevonPartridgeOfficer@users.noreply.github.com>
2023-10-10 18:49:45 +1300

    ADD: Rough code outline of weapon checks

--- Studio-5-Unity/Assets/Scripts/Gameplay/EquipWeapon.cs
+++ Studio-5-Unity/Assets/Scripts/Gameplay/EquipWeapon.cs
@@ -1,6 +1,11 @@
+/*
+This code is a work in-progress
+It mostly just contains the basic framework and my though process of how this should work at this stage of development
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class EquipWeapon : MonoBehaviour
 {
@@ -8,12 +13,11 @@ public class EquipWeapon : MonoBehaviour
     [Header("Player")]
     [Header("Weapons")]
     public GameObject playerWeapon;
-    [Header("Levels and XP")]
-    public int playerXP;
+    [Header("Levels")]
     public int playerLevel;
     public int currentWeaponLevel;
     [Header("Boolean States")]
-    public bool weaponEquipped;
+    public bool hasWeapon;
 
     [Header("")] //Additional spacing of inspector groups
 
@@ -29,12 +33,69 @@ public class EquipWeapon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Update in the future with GetRequests from the API (currently placeholders)
+        //eg        playerLevel = Get(Character.level)
+        //   currentWeaponLevel = Get(Character.item.level)
+        playerLevel = 2;
+        currentWeaponLevel = 1;
+        hasWeapon = WeaponEnabledCheck(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EquipWeaponCheck();
+    }
+
+    /// <summary>
+    /// If the player has already equipped an item or not
+    /// </summary>
+    /// <returns>(Type:Bool)  Checks if the players current weapon gameobject is active in the inspector</returns>
+    private bool WeaponEnabledCheck()
+    {
+        //NEED TO ADD MORE TO THIS
+
+        return playerWeapon.gameObject.activeSelf;
+    }
+
+    /// <summary>
+    /// Gets the level of the weapon in the current location
+    /// </summary>
+    /// <returns>(Type:int) Level of weapon returned from API Get request</returns>
+    private int GetWeaponLevel()
+    {
+        int itemLevel;
+        //itemLevel = Get(Location.item.level); REPLACE API REQUEST CODE HERE
+        itemLevel = 4;
+        return itemLevel;
+    }
+
+    /// <summary>
+    /// Gets the current level of the item you want to pickup
+    /// Checks if the player level is equal to or grater than the item level
+    /// Enables the characters weapon in the inspector
+    /// Swaps the player weapon gameobject to the pickup item
+    /// </summary>
+    private void EquipWeaponCheck()
+    {
+        pickupLevel = GetWeaponLevel();
+
+        if (playerLevel >= pickupLevel)
+        {
+            canEquip = true;
+                
+            if (!WeaponEnabledCheck())
+            {
+                playerWeapon.SetActive(true); 
+            }
+            hasWeapon = WeaponEnabledCheck();
+            playerWeapon = pickupWeapon; //Swap the player weapon gameobject
+            Debug.Log("You successfully pickup the weapon");
+        }
+        //LOOK AT ADDING A PLAYER CHECK IF THEY WANT TO SWAP WEAPON OR NOT
+        else
+        {
+            Debug.Log("Your character is not a high enough level to pick this up");
+        }
     }
 }
