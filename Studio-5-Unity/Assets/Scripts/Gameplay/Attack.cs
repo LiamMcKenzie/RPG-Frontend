b09eb11b519661600ff235c2c3bf909f0587b518
Devon Partridge-Officer <90590089+DevonPartridgeOfficer@users.noreply.github.com>
2023-10-24 15:08:29 +1300

    FIX: Attacks code changed to bool coroutine, attacks given avatar mask so only affect upper body. All working except bow rotation

--- Studio-5-Unity/Assets/Scripts/Gameplay/Attack.cs
+++ Studio-5-Unity/Assets/Scripts/Gameplay/Attack.cs
@@ -1,47 +1,56 @@
+using System.Buffers.Text;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Attack : MonoBehaviour
 {
     private Animator animator;
 
+    /// <summary>
+    /// Initialize the Animator component of the character.
+    /// </summary>
     private void Start()
     {
-        // Get the Animator component of your character.
         animator = GetComponent<Animator>();
     }
 
+    /// <summary>
+    /// Check for key presses and trigger corresponding animations based on key-to-animationBool mapping.
+    /// </summary>
     private void Update()
     {
-        // Axe Downward Swing
-        if (Input.GetKey(KeyCode.Z)) 
+        foreach (var kvp in attackKeyMap)
         {
-            animator.SetTrigger("Axe_Attack_Swing"); // Trigger animation toggle
-        }
-
-        //Axe Side Slash
-        if (Input.GetKey(KeyCode.X))
-        {
-            animator.SetTrigger("Axe_Attack_Slash");
-        }
-
-        //Staff Cast Attack
-        if (Input.GetKey(KeyCode.C))
-        {
-            animator.SetTrigger("Staff_Attack_Cast");
-        }
-
-        //Bow Idle Attack
-        if (Input.GetKey(KeyCode.V))
-        {
-            animator.SetTrigger("Bow_Attack_Idle");
-        }
-
-        //Dancing Animation
-        if (Input.GetKey(KeyCode.P))
-        {
-            animator.SetTrigger("Dance_01");
+            if (Input.GetKeyDown(kvp.Key))
+            {
+                StartCoroutine(TriggerAnimation(kvp.Value));
+            }
         }
     }
+
+    /// <summary>
+    /// Animation bool condition set to true and returns to false after a delay 
+    /// </summary>
+    /// <param name="animationCondition">Boolean name (kvp.Value) used in the character animator (e.g Axe_Slash)</param>
+    /// <returns></returns>
+    IEnumerator TriggerAnimation(string animationCondition)
+    {
+        animator.SetBool(animationCondition, true);
+        yield return new WaitForSeconds(0.1f);
+        animator.SetBool(animationCondition, false);
+    }
+
+    /// <summary>
+    /// A dictionary that maps KeyCodes to animation conditions for triggering specific attacks.
+    /// </summary>
+    private Dictionary<KeyCode, string> attackKeyMap = new Dictionary<KeyCode, string>
+    {
+        { KeyCode.Z, "Axe_Swing" },
+        { KeyCode.X, "Axe_Slash" },
+        { KeyCode.C, "Staff_Cast" },
+        { KeyCode.V, "Bow_Fire" },
+        { KeyCode.P, "Dance" }
+    };
 }
